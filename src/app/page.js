245e0f43b27df1468152caf5d05a495bdbbc5b64
AhmedHKhan18 <ahmed.18hassan.18@gmail.com>
2024-09-19 12:48:39 +0500

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -2,35 +2,36 @@
 import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [color, setColor] = useState("white");
-  const [yellowcolor, setYellowColor] = useState("white");
-  const [greencolor, setGreenColor] = useState("white");
-    setInterval(() => {
-      if (color === "white" && yellowcolor === "white" && greencolor === "white") {
-        setColor("red");
-      } else if (color === "red") {
-        setColor("white");
-        setYellowColor("yellow");
-      } else if (yellowcolor === "yellow") {
-        setGreenColor("green");
-        setYellowColor("white");
-        setColor("white");
-      } else if (greencolor === "green") {
-        setColor("white")
-        setYellowColor("white")
-        setGreenColor("white")
+  const [color, setColor] = useState("invisible");
+  const [yellowcolor, setYellowColor] = useState("invisible");
+  const [greencolor, setGreenColor] = useState("invisible");
+  function lights(){  
+  setInterval(() => {
+      if(color === "invisible" && yellowcolor === "invisible"){
+        setColor("visible")
+        setYellowColor("invisible")
+        setGreenColor("invisible")
+      }else if(yellowcolor === "invisible"){
+        setYellowColor("visible")
+        setColor("invisible")
+        setGreenColor("invisible")
+      }else if(greencolor === "invisible"){
+        setGreenColor("visible")
+        setColor("invisible")
+        setYellowColor("invisible")
       }
     }, 3000)
-
+  }
+  lights()
 
 
   return (
-    <div className="flex items-center justify-center flex-col">
+    <div className="flex items-center min-h-screen justify-center flex-col">
     <div className="mt-[10%] h-[53vh] w-[25vw] flex flex-col justify-between items-center">
       <img className="absolute mt-[-6%]" src="https://images.vexels.com/content/132502/preview/traffic-signal-light-sign-6145f9.png"/>
-    <div className={`bg-${color}-500 h-[17vh] w-[17vh] rounded-full`}>Re</div>
-    <div className={`bg-${yellowcolor}-500 h-[17vh] w-[17vh] rounded-full`}>yell</div>
-    <div className={`bg-${greencolor}-500 h-[17vh] w-[17vh]  rounded-full`}>gre</div>
+    <div className={`bg-red-500 h-[17vh] w-[17vh] rounded-full ${color}`}>Re</div>
+    <div className={`bg-yellow-500 h-[17vh] w-[17vh] rounded-full ${yellowcolor}`}>yell</div>
+    <div className={`bg-green-500 h-[17vh] w-[17vh]  rounded-full ${greencolor}`}>gre</div>
   </div>
     </div>
   );
